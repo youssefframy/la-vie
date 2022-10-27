2ebe1527fe025575b0f5420652c14da5c77b5d54
Youssef <youssefframy>
2022-10-27 20:54:54 +0200

    About Section

---
+++ src/components/homepage/About.tsx
@@ -0,0 +1,32 @@
+import AboutImage from '../../assets/about.png';
+
+const About : React.FC = () => {
+    return (
+      <div className="flex items-center justify-between space-x-2 my-20 mx-10">
+        <div className="w-[60%] pb-5">
+          <h1 className="text-3xl font-bold mb-3">About us</h1>
+          <h1 className="text-s w-[100%] text-darkgray pl-5 leading-7">
+            Welcome to La Vie, your number one source for planting. We're
+            dedicated to giving you the very best of plants, with a focus on
+            dependability, customer service and uniqueness. Founded in 2020, La
+            Vie has come a long way from its beginnings in a home office our
+            passion for helping people and give them some advices about how to
+            plant and take care of plants. We now serve customers all over
+            Egypt, and are thrilled to be a part of the eco-friendly wing{" "}
+            Welcome to La Vie, your number one source for planting. We're
+            dedicated to giving you the very best of plants, with a focus on
+            dependability, customer service and uniqueness. Founded in 2020, La
+            Vie has come a long way from its beginnings in a home office our
+            passion for helping people and give them some advices about how to
+            plant and take care of plants. We now serve customers all over
+            Egypt, and are thrilled to be a part of the eco-friendly wing{" "}
+          </h1>
+        </div>
+        <div className='border border-green rounded-lg flex justify-center items-center w-[25rem] h-[25rem]'>
+            <img src={AboutImage} alt="About" className='relative w-[25rem] h-[25rem] right-[-1rem] top-[-1rem]' />
+        </div>
+      </div>
+    );
+}
+
+export default About;
--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,11 +1,9 @@
-import { Link } from "react-router-dom";
-
 import Navbar from "../components/Navbar";
 import Splash from "../components/homepage/Splash";
 import Categories from "../components/homepage/PopularCategories";
 import SnakeCategories from "../components/homepage/SnakeCategories";
 import Blogs from "../components/homepage/Blogs";
-
+import About from "../components/homepage/About";
 
 const Homepage : React.FC = () => {
     const homepageList : string[] = ['Home', 'Shop', 'Blog', 'About', 'Community'];
@@ -18,6 +16,7 @@ const Homepage : React.FC = () => {
         <Categories />
         <SnakeCategories />
         <Blogs />
+        <About />
       </div>
     );
 }
